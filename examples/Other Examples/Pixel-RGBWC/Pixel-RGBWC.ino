29139bec91c1917bd9334890ac053813e879f6b5
abackup <40161397+abackup@users.noreply.github.com>
2025-09-04 16:30:47 +0800

    Update Pixel-RGBWC.ino

--- examples/Other Examples/Pixel-RGBWC/Pixel-RGBWC.ino
+++ examples/Other Examples/Pixel-RGBWC/Pixel-RGBWC.ino
@@ -25,35 +25,33 @@
  *  
  ********************************************************************************/
  
-// HomeSpan Addressable RGBCW LED Example.  Demonstrates use of two separate Accessories to control a SINGLE
-// NeoPixel LED Strip.  The first controls the RGB LEDs and the second controls the Warm-White and Cool-White LEDs.
-// Both sets can be on at the same time. 
+// HomeSpan 可寻址 RGBCW LED 示例。演示如何使用两个独立的配件控制单个 NeoPixel LED 灯带。第一个配件控制 RGB LED，第二个配件控制暖白光和冷白光 LED。
+// 两组配件可以同时开启。
 //
-// Note use of Pixel::CCT() method to convert color temperature into values for the White LEDs.
+// 请注意，使用 Pixel::CCT() 方法将色温转换为白光 LED 的色温值。
 //
-// Also note that HomeKit uses the Mirek scale for color temperture instead of the Kelvin scale.  To convert from
-// one to the other, divide into 1 million:  Mirek=1.0e6/Kelvin and Kelvin=1.0e6/Mirek.
+// 另请注意，HomeKit 使用 Mirek 色温标度而非开尔文色温标度来表示色温。要将两者进行转换，请将两者除以 100 万：Mirek=1.0e6/开尔文，开尔文=1.0e6/Mirek。
 //
 
 #ifndef HS_FEATHER_PINS
-  #define NEOPIXEL_PIN    23        // CHANGE THIS PIN TO MATCH YOUR BOARD
+  #define NEOPIXEL_PIN    23        // 更改此引脚以匹配您的主板
 #else
   #define NEOPIXEL_PIN    F23       // FEATHER PIN MAPPING - FOR HOMESPAN DEVELOPER USE ONLY
 #endif
  
-#define NPIXELS         100       // number of controllable pixels in strand (which may be less than actual number of LEDs)
-#define WARM_LED_TEMP   3000      // temperature (in Kelvin) of Warm-White LED
-#define COOL_LED_TEMP   6500      // temperature (in Kelvin) of Cool-White LED
+#define NPIXELS          100    // 灯串中可控像素的数量（可能小于实际 LED 数量）
+#define WARM_LED_TEMP    3000   // 暖白光 LED 的温度（单位：开尔文）
+#define COOL_LED_TEMP    6500   // 冷白光 LED 的温度（单位：开尔文）
  
 #include "HomeSpan.h"
 
-Pixel pixel(NEOPIXEL_PIN,"GRBWC");     // create a global instance of a Pixel strand supporting RGB LEDs plus warm AND cool white LED
-Pixel::Color colorRGB;                 // create a global instance of a Pixel color to be used to store the RGB color
-Pixel::Color colorWC;                  // create a global instance of a Pixel color to be used to store the WC color
+Pixel pixel(NEOPIXEL_PIN,"GRBWC");     // 创建一个支持 RGB LED 以及暖白光和冷白光 LED 的像素链全局实例
+Pixel::Color colorRGB;                 // 创建一个像素颜色的全局实例，用于存储 RGB 颜色
+Pixel::Color colorWC;                  // 创建一个像素颜色的全局实例，用于存储 WC 颜色
 
 ///////////////////////////////
 
-struct NeoPixel_RGB : Service::LightBulb {      // define an RGB Lightbulb to control the RGB LEDs on the NeoPixel Light Strip
+struct NeoPixel_RGB : Service::LightBulb {      // 定义一个 RGB 灯泡来控制 NeoPixel 灯带上的 RGB LED
  
   Characteristic::On power{0,true};
   Characteristic::Hue H{0,true};
@@ -63,7 +61,7 @@ struct NeoPixel_RGB : Service::LightBulb {      // define an RGB Lightbulb to co
   NeoPixel_RGB() : Service::LightBulb(){
     
     V.setRange(5,100,1);
-    update();                                 // manually call update() to set pixel with restored initial values
+    update();                                 // 手动调用 update() 来设置像素并恢复其初始值
   }
 
   boolean update() override {
@@ -76,7 +74,7 @@ struct NeoPixel_RGB : Service::LightBulb {      // define an RGB Lightbulb to co
 
 ///////////////////////////////
 
-struct NeoPixel_WC : Service::LightBulb {      // define WC Lightbulb to control the Warm White and Cool White LEDs on the NeoPixel Light Strip
+struct NeoPixel_WC : Service::LightBulb {      // 定义 WC 灯泡来控制 NeoPixel 灯带上的暖白和冷白 LED
  
   Characteristic::On power{0,true};
   Characteristic::Brightness V{100,true};
